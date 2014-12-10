2e9946214e217139f97c66702fc2150977166c4a
ryfighter <ryosbaldeston@gmail.com>
2014-12-10 08:50:01 -0500

    s

--- Abilities.c
+++ Abilities.c
@@ -8,21 +8,7 @@ class Abilities {
 	// Base Abilities
 	Ability terra, hydras, aeris, fire, frost, light, strength, step, sense, blink, manifestation, sight, mend, transference, shift;
 	
-	// Dual Abilities
-	Ability terra_hydras, terra_aeris, terra_fire, terra_frost, terra_light, terra_strength, terra_step, terra_sense, terra_blink, terra_manifestation, terra_sight, terra_mend, terra_transference, terra_shift;
-	Ability hydras_aeris, hydras_fire, hydras_frost, hydras_light, hydras_strength, hydras_step, hydras_sense, hydras_blink, hydras_manifestation, hydras_sight, hydras_mend, hydras_transference, hydras_shift;
-	Ability aeris_fire, aeris_frost, aeris_light, aeris_strength, aeris_step, aeris_sense, aeris_blink, aeris_manifestation, aeris_sight, aeris_mend, aeris_transference, aeris_shift;
-	Ability fire_frost, fire_light, fire_strength, fire_step, fire_sense, fire_blink, fire_manifestation, fire_sight, fire_mend, fire_transference, fire_shift;
-	Ability frost_light, frost_strength, frost_step, frost_sense, frost_blink, frost_manifestation, frost_sight, frost_mend, frost_transference, frost_shift;
-	Ability light_strength, light_step, light_sense, light_blink, light_manifestation, light_sight, light_mend, light_transference, light_shift;
-	Ability strength_step, strength_sense, strength_blink, strength_manifestation, strength_sight, strength_mend, strength_transference, strength_shift;
-	Ability step_sense, step_blink, step_manifestation, step_sight, step_mend, step_transference, step_shift;
-	Ability sense_blink, sense_manifestation, sense_sight, sense_mend, sense_transference, sense_shift;
-	Ability blink_manifestation, blink_sight, blink_mend, blink_transference, blink_shift;
-	Ability manifestation_sight, manifestation_mend, manifestation_transference, manifestation_shift;
-	Ability sight_mend, sight_transference, sight_shift;
-	Ability mend_transference, mend_shift;
-	Ability transference_shift;	
+	
 	
 	
 	
--- Actor.c
+++ Actor.c
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 class Actor{
 	string name;
 	Stats stats;
 	Abilities abilities;
+	map<string,int> primes;
 	public:
 		void _init_(string);
 };
@@ -13,4 +15,19 @@ class Actor{
 void Actor::_init_(n){
 	name = n;
 	abilities = new Abilities(this);
+	primes['terra'] = 0;
+	primes['hydras'] = 0;
+	primes['aeris'] = 0;
+	primes['fire'] = 0;
+	primes['frost'] = 0;
+	primes['light'] = 0;
+	primes['strength'] = 0;
+	primes['step'] = 0;
+	primes['sense'] = 0;
+	primes['blink'] = 0;
+	primes['manifestation'] = 0;
+	primes['sight'] = 0;
+	primes['mend'] = 0;
+	primes['transference'] = 0;
+	primes['shift'] = 0;
 }
